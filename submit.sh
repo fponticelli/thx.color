8e50a65d76e4667ac406d0e70c11361c17912585
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-10-13 16:46:52 -0600

    release

--- submit.sh
+++ submit.sh
@@ -1,3 +1,3 @@
 rm thx.color.zip
-zip -r thx.color.zip hxml src test extraParams.hxml haxelib.json README.md demo demo.hxml
+zip -r thx.color.zip hxml src test doc/ImportColor.hx extraParams.hxml haxelib.json README.md demo demo.hxml
 haxelib submit thx.color.zip
