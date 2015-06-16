7b28819730b348b3fa66201b2fb68e17c1451d5e
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-06-15 20:46:01 -0600

    updated Demo

--- demo/Demo.hx
+++ demo/Demo.hx
@@ -11,76 +11,83 @@ class Demo {
         right : Hsv = 'hsv(345deg,88%,77%)';
 
     MiniCanvas.create(400, 20).gradientHorizontal(
-      function(t : Float) : Rgba return (left : Rgb).interpolate(right, t))
+      function(t : Float) : Rgbxa return (left : Rgb).interpolate(right, t))
       .display('interpolateRgb');
 
     MiniCanvas.create(400, 20).gradientHorizontal(
-      function(t : Float) : Rgba return (left : Cmy).interpolate(right, t))
+      function(t : Float) : Rgbxa return (left : Cmy).interpolate(right, t))
       .display('interpolateCmy');
 
     MiniCanvas.create(400, 20).gradientHorizontal(
-      function(t : Float) : Rgba return (left : Cmyk).interpolate(right, t))
+      function(t : Float) : Rgbxa return (left : Cmyk).interpolate(right, t))
       .display('interpolateCmyk');
 
     MiniCanvas.create(400, 20).gradientHorizontal(
-      function(t : Float) : Rgba return (left : Grey).interpolate(right, t))
+      function(t : Float) : Rgbxa return (left : Grey).interpolate(right, t))
       .display('interpolateGrey');
 
     MiniCanvas.create(400, 20).gradientHorizontal(
-      function(t : Float) : Rgba return (left : Hsl).interpolate(right, t))
+      function(t : Float) : Rgbxa return (left : Hsl).interpolate(right, t))
       .display('interpolateHsl');
 
     MiniCanvas.create(400, 20).gradientHorizontal(
-      function(t : Float) : Rgba return (left : Hsv).interpolate(right, t))
+      function(t : Float) : Rgbxa return (left : Hsv).interpolate(right, t))
       .display('interpolateHsv');
 
     MiniCanvas.create(400, 20).gradientHorizontal(
-      function(t : Float) : Rgba return (left : CieLab).interpolate(right, t))
+      function(t : Float) : Rgbxa return (left : CieLab).interpolate(right, t))
       .display('interpolateCielab');
 
-    MiniCanvas.create(400, 20).gradientHorizontal(
-      function(t : Float) : Rgba return (left : CieLCh).interpolate(right, t))
+    MiniCanvas.create(400, 20)
+      .checkboard()
+      .gradientHorizontal(function(t : Float) : Rgbxa return (left : CieLCh).interpolate(right, t))
       .display('interpolateCielch');
 
     MiniCanvas.create(400, 20).gradientHorizontal(
-      function(t : Float) : Rgba return (left : Xyz).interpolate(right, t))
+      function(t : Float) : Rgbxa return (left : Xyz).interpolate(right, t))
       .display('interpolateXyz');
 
     MiniCanvas.create(400, 20).gradientHorizontal(
-      function(t : Float) : Rgba return (left : Yxy).interpolate(right, t))
+      function(t : Float) : Rgbxa return (left : Yxy).interpolate(right, t))
       .display('interpolateYxy');
   }
 
   public static function main() {
     interpolations();
 
+    var t = Rgbxa.create(0, 0, 0, 0);
+
     MiniCanvas.create(200, 200).box(
-      function(x : Float, y : Float) : Rgba
+      function(x : Float, y : Float) : Rgbxa
         return Hsl.create(x * 360, 1, y))
       .display("rainbowHsl");
 
-    MiniCanvas.create(200, 200).box(
-      function(x : Float, y : Float) : Rgba
-        return CieLCh.create(65, y * 65, x * 360))
+    MiniCanvas.create(200, 200)
+      .checkboard()
+      .box(function(x : Float, y : Float) : Rgbxa
+        return CieLCh.create(75, y * 65, x * 360))
       .display("rainbowCielch");
 
-    MiniCanvas.create(200, 200).box(
-      function(x : Float, y : Float) : Rgba
-        return CieLab.create(40, x * 200 - 100, y * 200 - 100))
+    MiniCanvas.create(200, 200)
+      .checkboard()
+      .box(function(x : Float, y : Float) : Rgbxa
+        return CieLab.create(75, x * 256 - 128, y * 256 - 128))
       .display("rainbowCielab");
 
-    MiniCanvas.create(200, 200).box(
-      function(x : Float, y : Float) : Rgba
-        return Xyz.create(x * 800, y * 900, 1)
-    ).display("xyzChromaticityDiagram");
+    MiniCanvas.create(200, 200)
+      .checkboard()
+      .box(
+        function(x : Float, y : Float) : Rgbxa
+          return Xyz.create(x * 8 + 4, y * 8 + 4, 10))
+      .display("xyzChromaticityDiagram");
 
     MiniCanvas.create(400, 20).gradientHorizontal(
-      function(t) : Rgba
+      function(t) : Rgbxa
         return ('#ff0000' : Rgb).darker(t))
       .display('darkerRgb');
 
     MiniCanvas.create(400, 20).gradientHorizontal(
-      function(t) : Rgba
+      function(t) : Rgbxa
         return ('#0000ff' : Rgb).lighter(t))
       .display('lighterRgb');
 
@@ -99,9 +106,9 @@ class Demo {
     ctx.font = '${Math.round(cellh*0.4)}px Verdana, sans-serif';
     colors.mapi(function(name, i) {
       var col   = i % columns,
-        row   = Math.floor(i / columns),
-        color = Color.names.get(name);
-
+          row   = Math.floor(i / columns),
+          color = Color.names.get(name);
+      trace(color);
       ctx.fillStyle = color.toString();
       ctx.fillRect(col * cellw, row * cellh, cellw, cellh);
 
