8db0230e9333ff495ae3b950656a33d6aad78508
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-06-17 22:35:48 -0600

    exposing bugs using tests

--- test/thx/color/TestRoundTrip.hx
+++ test/thx/color/TestRoundTrip.hx
@@ -21,7 +21,8 @@ class TestRoundTrip {
 
     colors
       .map.fn({
-        rgbx: _,
+        rgbx: (_ : Rgbx),
+        rgb:  (_ : Rgb),
         lab:  (_ : CieLab),
         lch:  (_ : CieLCh),
         cmy:  (_ : Cmy),
@@ -52,17 +53,32 @@ class TestRoundTrip {
       .map.fn({
         var t : CieLab = _.lab.toString();
         Assert.isTrue(_.lab == t, 'expected ${_.lab} but was $t');
-      /*
-
         var t : CieLCh = _.lch.toString();
         Assert.isTrue(_.lch == t, 'expected ${_.lch} but was $t');
-
+        var t : Cmy = _.cmy.toString();
+        Assert.isTrue(_.cmy == t, 'expected ${_.cmy} but was $t');
+        var t : Cmyk = _.cmyk.toString();
+        Assert.isTrue(_.cmyk == t, 'expected ${_.cmyk} but was $t');
+        var t : CubeHelix = _.ch.toString();
+        Assert.isTrue(_.ch == t, 'expected ${_.ch} but was $t');
+        var t : Grey = _.g.toString();
+        Assert.isTrue(_.g == t, 'expected ${_.g} but was $t');
+        var t : Hcl = _.hcl.toString();
+        Assert.isTrue(_.hcl == t, 'expected ${_.hcl} but was $t');
         var t : Hsl = _.hsl.toString();
         Assert.isTrue(_.hsl == t, 'expected ${_.hsl} but was $t');
-      */
+        var t : Hsv = _.hsv.toString();
+        Assert.isTrue(_.hsv == t, 'expected ${_.hsv} but was $t');
+        var t : Rgbx = _.rgbx.toString();
+        Assert.isTrue(_.rgbx == t, 'expected ${_.rgbx} but was $t');
+        var t : Xyz = _.xyz.toString();
+        Assert.isTrue(_.xyz == t, 'expected ${_.xyz} but was $t');
+        var t : Yxy = _.yxy.toString();
+        Assert.isTrue(_.yxy == t, 'expected ${_.yxy} but was $t');
+
         return _;
       })
-      .map.fn(trace(_))
+      //.map.fn(trace(_))
      ;
   }
 
