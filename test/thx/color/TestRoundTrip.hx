5abe2168a51b433186c48211f64d2b8c910ca342
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-06-18 23:26:33 -0600

    fixed tests (there are some rounding issues but the logic seems ok)

--- test/thx/color/TestRoundTrip.hx
+++ test/thx/color/TestRoundTrip.hx
@@ -30,9 +30,9 @@ class TestRoundTrip {
       Rgbx.create(0.05, 0.10, 0.15), // pure black is not guaranteed to make a clean roundtrip
       Rgbx.create(1, 0, 0),
       Rgbx.create(0, 1, 0),
-      Rgbx.create(0, 0, 1),
-      Rgbx.create(0.9, 0.95, 1), // pure white is not guaranteed to make a clean roundtrip
-      Rgbx.create(0.2, 0.4, 0.5)
+      Rgbx.create(0.2, 0.2, 1),
+      Rgbx.create(0.9, 0.9, 1), // pure white is not guaranteed to make a clean roundtrip
+      Rgbx.create(0.2, 0.4, 0.52)
     ];
 
     tests = colors
